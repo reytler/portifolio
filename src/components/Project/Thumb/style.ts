35f68895933135a15f6a1ab920fc33d428717139
Reytler Souza <reytlercompare@hotmail.com>
2024-01-18 16:45:31 -0300

    Add media query form thumb

--- src/components/Project/Thumb/style.ts
+++ src/components/Project/Thumb/style.ts
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    padding: 15px;
+    padding: 15px 0 15px 0;
     cursor: pointer;
     img{
         width: 418px;
         height: 304px;
+        @media (max-width: 425px) {
+            width: 393px;
+            height: 276px;
+        }
+
+        @media (max-width: 375px) {
+            width: 347px;
+            height: 230px;
+        }
+        
+        @media (max-width: 320px) {
+            width: 296px;
+            height: 230px;
+        }
     }
 `;
